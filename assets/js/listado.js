b815f3a64b42ab424c165fa6d0104f266c950758
Omar Soto <omarsotosainz@gmail.com>
2022-09-01 17:38:43 -0600

    DevF - Modulo 3 - Version Final

--- assets/js/listado.js
+++ assets/js/listado.js
@@ -185,3 +185,39 @@ let masInfoPrincipal = document.getElementById('masInfoPrincipal');
 masInfoPrincipal.addEventListener('click', (e) => {
     location.href = 'single.html?movie_id='+itemPrincipal;
 })
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtnLogout");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+// Get the button that opens the modal
+var btnCerrarSesion = document.getElementById("cerrarSesion");
+btnCerrarSesion.onclick = function() {
+    localStorage.removeItem('tmbd_user_token');
+    console.log("Cerro sesion");
+    modal.style.display = "none";
+    location.href = 'login.html';
+}
